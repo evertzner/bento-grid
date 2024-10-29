40015e0c27090a5fb358b71698698bc810365151
Esteban Vertzner <esteban.vertzner@canda.com>
2024-10-29 14:30:25 +0100

    finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       screens: {
-        '2xl': '1440px'
+        '2xl': '1440px',
+        '3xl': '2100px'
       },
       colors: {
         purple: {
@@ -19,6 +20,12 @@ export default {
         'wild-sand': { 50: '#F6F5F6' }
       },
       spacing: {
+        13: '3.25rem', // 52px
+        15: '3.75rem', // 60px
+        18: '4.5rem', // 72px
+        19: '4.75rem', // 76px
+        21: '5.25rem', // 84px
+        41: '10.25rem', // 164px
         45: '11.25rem', // 180px
         47.5: '11.875rem', // 190px
         50: '12.5rem', // 200px
@@ -26,6 +33,7 @@ export default {
         57: '14.25rem', // 228px
         77: '19.25rem', // 308px
         79: '19.75rem', // 316px
+        89: '22.25rem', // 356px
         100: '0.5rem', // 8px
         200: '1rem', // 16px
         300: '1.5rem', // 24px
